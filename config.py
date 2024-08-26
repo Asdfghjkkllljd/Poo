f6c729b1fa8040b8bd480afcdff62030ab764d3a
Asdfghjkkllljd <staricinal49@gmail.com>
2024-08-27 00:31:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ TOKEN = "7429518608:AAFMU2R_CyyLqPhWl2LJTWxTQVPU1DhSppk" # Токен бота
 CHANNEL_ID = -1002243436081 # ID канала со ставками
 ADMINS = [5846969551] # ID админов
 BET_URL = "t.me/send?start=IVpAqLJ8Pztg" # Ссылка на счет
-CRYPTOPAY_TOKEN = "251801:AAaOA42R6B4OjqU4ciwBt2KRjmAHfyPx6mY" # Тут CryptoPay токен
+CRYPTOPAY_TOKEN = "257530:AAjBbpqNi7zCgG9RQwAlTDLOFRdKySJ2N6Q" # Тут CryptoPay токен
 WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
 LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
 LOGS_ID = -1002226990800 # ID канала с оплатами
